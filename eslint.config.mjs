974cf89b51dc1f981a0318931263b28385edfbfe
Katerina Chernova <chkaterina1@gmail.com>
2025-05-11 19:50:38 +0400

    disable stylistic rules in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -52,6 +52,9 @@ export default [
       'import/no-named-as-default-member': 'off',
       'no-console': 'off',
       'import/no-extraneous-dependencies': 'off',
+      '@stylistic/semi': 'off',
+      '@stylistic/arrow-parens': 'off',
+      '@stylistic/brace-style': 'off',
     },
   },
 ];
